6f2637d39186ff05e43fa116c75f5b401b13ca51
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-10-17 23:00:31 -0300

    Página de Não Encontrado criada.

---
+++ src/app/not-found.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>OOPS! 404</h1>
+        <p>Not found!</p>
+        <div>
+            <Image
+            src="https://httpstatusdogs.com/img/404.jpg"
+            alt="Cachorro cavando."
+            width={500}
+            height={500}/>
+        </div>
+    </div>
+  )
+}
