772b2f239b9355cf121a30ec2cf1e48b63c35a1e
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-10-31 21:54:16 -0300

    GET ALL e GET BY ID em users

---
+++ src/app/api/base/base-users/[id]/route.jsx
@@ -0,0 +1,19 @@
+import {promises as fs} from "fs"
+import { NextResponse } from "next/server"
+
+
+export async function GET(request, {params}) {
+    
+    const file = await fs.readFile(process.cwd() + "/src/app/api/base/data.json", "utf-8")
+
+    const id = params.id;
+
+    const usuarios = JSON.parse(file)
+
+    if(id > 0 && id <= usuarios.usuarios.lenght){
+        return NextResponse.json(usuarios.usuarios.find((user) => user.id == id)) 
+    } else {
+        return id == 0 ? NextResponse.json(usuarios.usuarios) : NextResponse.redirect("http://localhost:3000/error")
+    }
+
+}
